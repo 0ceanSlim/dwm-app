68127bf05c4537c6cb82b56d5d6f9afe7992f9f9
0ceanSlim <chriskerr@gmx.com>
2024-02-01 12:40:14 -0500

    sprites added

---
+++ static/js/sprite.js
@@ -0,0 +1,30 @@
+function updateMonsterSprite() {
+    var selectedMonster = document.getElementById("monsterDropdown").value;
+    var spriteUrl = `static/img/monster/${selectedMonster}.png`;
+    
+    var iframe = document.getElementById("monsterSpriteIframe");
+    iframe.src = "about:blank"; // Clear the iframe content
+    
+    var content = `<style>
+                        body {
+                            margin: 0;
+                            display: flex;
+                            justify-content: center;
+                            align-items: center;
+                            height: 100vh;
+                        }
+
+                        img {
+                            max-width: 100%;
+                            max-height: 100%;
+                            object-fit: contain;
+                        }
+                    </style>
+                    <body>
+                        <img src="${spriteUrl}" alt="Monster Sprite">
+                    </body>`;
+    
+    iframe.contentWindow.document.open();
+    iframe.contentWindow.document.write(content);
+    iframe.contentWindow.document.close();
+}
