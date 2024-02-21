d6eb52bb3c969b5cf3319904bec80d277f01ad9d
0ceanSlim <chriskerr@gmx.com>
2024-02-21 12:38:00 -0500

    styling, repalced app img

--- static/js/updateMonsterFamily.js
+++ static/js/updateMonsterFamily.js
@@ -1,16 +1,22 @@
 function updateMonsterFamily() {
     // Get the selected monster from the dropdown
     var selectedMonster = document.getElementById("monsterDropdown").value;
-
+  
     // Make an API request to get the stats for the selected monster
     fetch(`/api/monster/stats?monster=${selectedMonster}`)
       .then(response => response.json())
       .then(data => {
-        // Update the HTML content with the monster family
+        // Function to capitalize the first letter
+        function capitalizeFirstLetter(string) {
+          return string.charAt(0).toUpperCase() + string.slice(1);
+        }
+  
+        // Update the HTML content with the capitalized monster family
         var familyContainer = document.getElementById("monsterFamilyContainer");
-        familyContainer.innerHTML = `<h2>Family: ${data.Family}</h2>`;
+        familyContainer.innerHTML = `<h2>Family: ${capitalizeFirstLetter(data.Family)}</h2>`;
       })
       .catch(error => {
         console.error("Error fetching monster family:", error);
       });
   }
+  
--- static/js/updateMonsterStats.js
+++ static/js/updateMonsterStats.js
@@ -8,15 +8,17 @@ function updateMonsterStats() {
       .then(data => {
         // Update the HTML content with the monster stats
         var statsContainer = document.getElementById("monsterStatsContainer");
-        statsContainer.innerHTML = `<p>Max Level: ${data["Max Level"]}</p>
-                                    <p>Experience: ${data.Experience}</p>
-                                    <p>Health Points: ${data["Health Points"]}</p>
-                                    <p>Mana Points: ${data["Mana Points"]}</p>
-                                    <p>Attack: ${data.Attack}</p>
-                                    <p>Defense: ${data.Defense}</p>
-                                    <p>Agility: ${data.Agility}</p>
-                                   <p>Intelligence: ${data.Intelligence}</p>
-                                   `;
+        statsContainer.innerHTML = `<div class="font-bold text-center text-xl">Stats</div>
+                                    <div class="grid grid-cols-4 mt-1 mb-1 gap-x-2">
+                                    <p class="font-bold">Max Level:                                 </p><p class="font-bold text-violet-500">${data["Max Level"]}</p>
+                                    <p class="font-bold">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;EXP:   </p><p class="font-bold text-violet-500">${data.Experience}</p>
+                                    <p class="font-bold">&nbsp;&nbsp;&nbsp;&nbsp;HP:&nbsp;          </p><p class="font-bold text-violet-500">${data["Health Points"]}</p>
+                                    <p class="font-bold">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ATK:   </p><p class="font-bold text-violet-500">${data.Attack}</p>
+                                    <p class="font-bold">&nbsp;&nbsp;&nbsp;&nbsp;MP:&nbsp;          </p><p class="font-bold text-violet-500">${data["Mana Points"]}</p>
+                                    <p class="font-bold">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;DEF:   </p><p class="font-bold text-violet-500">${data.Defense}</p>
+                                    <p class="font-bold">&nbsp;&nbsp;&nbsp;&nbsp;AGL:               </p><p class="font-bold text-violet-500">${data.Agility}</p>
+                                    <p class="font-bold">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;INT:   </p><p class="font-bold text-violet-500">${data.Intelligence}</p>
+                                    `;
       })
       .catch(error => {
         console.error("Error fetching monster stats:", error);
