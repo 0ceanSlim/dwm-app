ce1a24217ae4f0e9a8facdd0de538c05ac0db4a6
0ceanSlim <chriskerr@gmx.com>
2024-02-21 09:27:05 -0500

    families and locations added to stats api

--- src/api/get_monster_stats.py
+++ src/api/get_monster_stats.py
@@ -11,23 +11,30 @@ def get_monster_stats():
     selected_monster = request.args.get("monster")
 
     if selected_monster:
-        # Fetch specific stats for the monster
-        cursor.execute("""
+# Fetch specific stats for the monster
+        query = """
             SELECT
-                name,
-                agl AS agility,
-                int AS intelligence,
-                maxlvl AS max_level,
-                exp AS experience,
-                hp AS health_points,
-                atk AS attack,
-                def AS defense
+                monsters.name,
+                monsters.agl AS agility,
+                monsters.int AS intelligence,
+                monsters.maxlvl AS max_level,
+                monsters.exp AS experience,
+                monsters.hp AS health_points,
+                monsters.atk AS attack,
+                monsters.def AS defense,
+                families.name AS family,
+                spawn_locations.map || ' - ' || spawn_locations.description AS location
             FROM monsters
-            WHERE LOWER(name) = LOWER(?)
-        """, (selected_monster.lower(),))
+            LEFT JOIN families ON monsters.family_id = families.id
+            LEFT JOIN spawn_locations ON monsters.id = spawn_locations.monster_id
+            WHERE LOWER(monsters.name) = LOWER(?)
+        """
+
+        cursor.execute(query, (selected_monster.lower(),))
 
         # Fetch the result and convert it to a dictionary
         monster_stats = cursor.fetchone()
+        print("Result:", monster_stats)
 
         if monster_stats:
             # Map stat names to descriptive labels
@@ -38,13 +45,15 @@ def get_monster_stats():
                 "attack": "Attack",
                 "defense": "Defense",
                 "agility": "Agility",
-                "intelligence": "Intelligence"            
-                }
+                "intelligence": "Intelligence",
+                "family": "Family",
+                "location": "Location"
+            }
 
             # Create a new dictionary with descriptive stat names
             formatted_stats = {
                 "name": monster_stats[0],
-                **{stat_labels[key]: monster_stats[i + 1] for i, key in enumerate(["agility", "intelligence", "max_level", "experience", "health_points", "attack", "defense"])}
+                **{stat_labels[key]: monster_stats[i + 1] for i, key in enumerate(["agility", "intelligence", "max_level", "experience", "health_points", "attack", "defense", "family", "location"])}
             }
 
             return jsonify(formatted_stats)
